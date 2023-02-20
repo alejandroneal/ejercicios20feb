ca31f95f85c81309aadb9fabe8a24a70801f3568
alejandroneal <aneachi128@g.educaand.es>
2023-02-20 17:39:10 +0100

    ejercicio a empezado

---
+++ src/main/java/ejercicios/EjercicioA.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicios;
+
+import javax.swing.JOptionPane;
+
+/**
+ * Leer un nº con JOption
+ * Controlar entradas por teclado
+ * Convertir nº leido en array de char
+ * Método recursivo indica si array contiene nº capicúa
+ */
+public class EjercicioA {
+    public static void main(String[] args) {
+        
+        //Pedimos un nº por teclado
+        int numero = pedirNumero();
+        
+        //Introducimos el nº en un array de char
+        char[] array = meterNumeroEnArray(numero);
+        
+        //Comprovamos si el nº es introducido en el array es capicúo
+    }
+    
+    public static int pedirNumero(){
+        int numero;
+        
+        do {
+            try {
+                numero = Integer.parseInt(JOptionPane.showInputDialog("Introduce un número entero"));
+                return numero;
+            } catch (NumberFormatException nfe) {
+                JOptionPane.showMessageDialog(null, "No se admiten caracteres no numéricos");
+            }
+        } while(true);
+    }
+    
+    public static char [] meterNumeroEnArray(int numero){
+        String numeroString = String.valueOf(numero);
+        char[] array = new char[numeroString.length()];
+        
+        for (int i = 0; i < numeroString.length(); i++) {
+            array[i] = numeroString.charAt(i);
+        }
+        
+        return array;
+    }
+    
+    public static boolean comprobarCapicuo(char [] array){
+        
+    }
+}

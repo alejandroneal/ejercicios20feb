d178c0a8d22ac534aef778d6502024a882714f76
alejandroneal <aneachi128@g.educaand.es>
2023-02-21 17:51:44 +0100

    ejercicio c planteado

---
+++ src/main/java/ejercicios/Botella.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicios;
+
+/**
+ * Clase padre: botella
+ * Hijas: agua, alcohol
+ * Método abstracto: servir
+ */
+public class Botella {
+    
+}

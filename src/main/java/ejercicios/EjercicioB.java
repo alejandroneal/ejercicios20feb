bbd3165c5c713b6f9d22dc556ac32f733cbb81be
alejandroneal <aneachi128@g.educaand.es>
2023-02-21 17:33:41 +0100

    ejercicio b empezado

---
+++ src/main/java/ejercicios/EjercicioB.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicios;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Alejandro
+ */
+public class EjercicioB {
+    public static void main(String[] args) {
+        
+        String palabra = pedirPalabra();
+        
+        mostrarPalabrasYTamanio(palabra);
+    }
+    
+    public static String pedirPalabra(){
+        return JOptionPane.showInputDialog("Introduce una palabra");
+    }
+    
+    public static void mostrarPalabrasYTamanio(String palabra){
+        String texto = """
+                       La palabra introducida es %s
+                       Su tamaño es de %s letras
+                       """.formatted(palabra, palabra.length());
+        JOptionPane.showMessageDialog(null, texto);
+    }
+}
